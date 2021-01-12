ed89137a6559f95d53df9b6d1ea65677edbf59dd
wooneeS2 <hello_world_@kakao.com>
2021-01-12 17:46:18 +0900

    errors....

--- dart_flutter/lib/main.dart
+++ dart_flutter/lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:ui';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/services.dart';
-//import 'package:foreground_service/foreground_service.dart';
+import 'package:foreground_service/foreground_service.dart';
 import 'package:android_intent/android_intent.dart';
 import 'package:android_intent/flag.dart';
 import 'package:intl/intl.dart';
@@ -15,7 +15,9 @@ import 'package:weather/weather.dart';
 
 
 void main(){
-  runApp(new AlarmWeather());
+  runApp(Phoneix(
+    child: new AlarmWeather(),
+  ));
 }
 
 class AlarmWeather extends StatefulWidget {
@@ -28,23 +30,176 @@ class AlarmWeatherState extends State<AlarmWeather> {
   Widget build(BuildContext context){
     return new MaterialApp(title: 'Flutter Project', home : MainPage(), routes: {
       '/page1' : (context) => MainPage(),
-      //'/page2' : (context) => SecondPage(),
+      '/page2' : (context) => SecondPage(),
     });
   }
 }
+Future<bool> removeAlarmInfo(int id, BuildContext context) async {
+  print('removeAlarmInfo()');
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int count = await getAlarmCount();
+  int i = 0;
+  for (i = id; i < count; i++) {
+    var myFuture = await getAlarmInfo(i);
+    var myNextFuture = await getAlarmInfo(i + 1);
+    var alarmID = await getAlarmID(myNextFuture[0]);
+
+    await saveAlarmInfo(
+        myFuture[0], myNextFuture[1], myNextFuture[2].format(context), myNextFuture[3]);
+
+    await saveAlarmID(myFuture[0], alarmID);
+
+    if (myNextFuture[0] == 100) {
+      break;
+    }
+  }
+  prefs.remove(i.toString());
+  removeAlarmID(i);
+
+  checkStopService();
+  return true;
+}
+
+void removeAlarmID(int listID) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove('alarm_id_' + listID.toString());
+}
+
+void checkStopService() async {
+  print ('checkStopService()');
+  int counterForAlarmOn = 0;
+  var count = await getAlarmCount();
+  for(int i = 0; i < coount ; i++){
+    var myFuture = await getAlarmInfo(i);
+    if(myFuture[3]) {
+      counterForAlarmOn++;
+    }
+  }
+
+  if(counterForAlarmOn == 0){
+    ForegroundService.stopForegroundService();
+  }
+
+}
+
+Future<bool> showAlertDialog(
+    BuildContext context, List<AlarmInfo> alarmInfo, int index) async =>
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('알람'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('알람을 삭제합니다.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () async {
+                await removeAlarmInfo(index, context);
+                AndroidAlarmManager.cancel(index);
+                stopAlarm();
+                Navigator.pop(context);
+                return true;
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+
+
+
 
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
+
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<AlarmInfo> alarmList = <AlarmInfo>[];
   int _curPageIndex = 0;
   String _weatherStr;
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = TimeOfDay.now();
+  bool _isAlarmOn = false;
+  int _alarmID = 0;
+  int _listIndex = 0;
+
+  Future<Null> getInitData() async {
+    var count = await getAlarmCount();
+    for (int i = 0; i < count; i++) {
+      var value = await getAlarmInfo(i);
+      if (!(value[0] == 100)) {
+        var alarmID = await getAlarmID(value[0]);
+        setState(() {
+          alarmList.insert(_listIndex, AlarmInfo(value[1], value[2], value[3]));
+          saveAlarmInfo(
+              _listIndex, value[1], value[2].format(context), value[3]);
+          saveAlarmID(_listIndex, alarmID);
+          _listIndex++;
+          saveAlarmCount(_listIndex);
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    print('initState()');
+    super.initState();
+    getInitData();
+  }
+
+
+
+
+  void addItemToList() {
+    setState(() {
+      _isAlarmOn = true;
+      saveAlarmInfo(_listIndex, _date, _time.format(context), _isAlarmOn);
+      alarmList.insert(_listIndex, AlarmInfo(_date, _time, _isAlarmOn));
+      _listIndex++;
+      saveAlarmCount(_listIndex);
+    });
+  }
+  ListView alarmPage(){
+    return ListView.builder(
+      padding:const Edge Insets.all(8),
+    itemCount: alarmList.length,
+    itemBuilder: (BuildContext context, int index){
+        if(alarmList. isEmpty){
+          return Text('Alarm is empty!');
+    }
+        return AlarmTile(alarmList, index);
+    }
+    );
+  }
+
+
+
+  Future<DateTime> selectDate(BuildContext context) {
+    return showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2999));
+  }
+
+  Future<TimeOfDay> selectTime(BuildContext context) {
+    return showTimePicker(context: context, initialTime: TimeOfDay.now());
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    //setRebirth(context);
+    setRebirth(context);
     return new Scaffold(
       appBar: new AppBar(
         title: const Text('Alarm and Weather'),
@@ -125,13 +280,112 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  ListView alarmPage() {
-    return ListView(
-      children: <Widget>[
-        Text('Empty....'),
-      ],
+
+
+
+  FloatingActionButton alarmAddButton() {
+    return FloatingActionButton(
+      tooltip: 'Add Alarm',
+      child: Icon(Icons.alarm_add),
+      onPressed: () {
+        selectDate(context).then((value) {
+          _date = value;
+          if (_date != null) {
+            selectTime(context).then((value) {
+              _time = value;
+              if (_time != null) {
+                _date = DateTime(_date.year, _date.month, _date.day, _time.hour,
+                    _time.minute);
+                setAlarmManager();
+                addItemToList();
+              } else {
+                _time = TimeOfDay.now();
+              }
+            });
+          } else {
+            _date = DateTime.now();
+          }
+        });
+      },
     );
   }
+
+  void setAlarmManager() async {
+    await AndroidAlarmManager.initialize();
+    await AndroidAlarmManager.oneShotAt(
+      _date,
+      _alarmId,
+      startAlarm,
+      exact: true,
+    );
+    await saveAlarmID(_listIndex - 1, _alarmID);
+    _alarmID++;
+    startService(_date);
+  }
+}
+
+
+void startAlarm() async{
+  print('startAlarm()');
+  AudioCache player = AudioCache();
+  AudioPlayer audioPlayer = await player.loop('good_morning.mp3');
+
+  ReceivePort receviePort = new ReceivePort();
+  IsolateNameServer.registerPortWithName(receivePort, sendPort,'player');
+  receivePort.listen((message){
+    if (audioPlayer == null) {
+      print('audioPlayer == null'){
+        print('audioPlayer is null');
+
+      }else {
+        audioPlayer.stop();
+    }
+    receivePort.close();
+      IsolateNameServer.removePortNameMapping('player');
+      print('player received $message');
+    }
+    );
+
+  SendPort sendPort = IsolatedNameServer.lookupPortByName('rebirth');
+  sendPort.send('start');
+
+}
+
+void setRebirth(BuildContext context) {
+    print('setRebirth()');
+
+    ReceivePort receivePort = new ReceivePort();
+    IsolateNameServer.removePortNameMapping('rebirth');
+    IsolateNameServer.registerPortWithName(receivePort.sendPort, 'rebirth');
+    receivePort.listen((message){
+      if(message == 'start'){
+        print('rebirth received $message');
+        AndroidIntent intent = AndroidIntent(
+          package: 'com.project.alarm_weather.MainActivity',
+          flags: <int>[
+            Flag.FLAG_ACTIVITY_CLEAR_TOP,
+            Flag.FLAG_ACTIVITY_SINGLE_TOP
+          ]
+        );
+        intent.launch();
+        Navigator.pushReplacement(context,'/page2');
+        receivePort.close();
+        IsolateNameServer.removePortNameMapping('rebirth');
+
+      }
+    });
+  }
+
+
+
+void stopAlarm(){
+    print('stopAlarm()');
+    SendPort sendPort = IsolatedNameServer.lookupPortByName('player');
+    if(sendPort != null){
+      sendPort.send('stop');
+    }
+  }
+
 
   Center weatherPage() {
     if (_weatherStr != null) {
@@ -221,5 +475,294 @@ class _MainPageState extends State<MainPage> {
     });
     return weather;
   }
+}
 
+
+void startService(DateTime dateTime) async {
+  if(!(await ForegroundService.foregroundServiceIsStarted())) {
+    await ForegroundService.setServiceIntervalSeconds(10);
+    await ForegroundService.notification.startEditMode();
+    await ForegroundService.notification.setTitle("등록된 알람");
+    await ForegroundService.notification.setText("$dateTime");
+    await ForegroundService.notification.finishEditMode();
+
+    await ForegroundService.startForegroundService(foregroundServiceFunction);
+    await ForegroundService.getWakeLock();
+
+  }
+
+  await ForegroundService.setupIsolateCommunication((data) {
+    debugPrint("main received: $data");
+  });
+}
+
+void foregroundServiceFunction() {
+  debugPrint("The current time is: ${DateTime.now()}");
+
+  if(!ForegroundService.isIsolateCommunicationSetup) {
+    ForegroundService.setupIsolateCommunication((data) {
+      debugPrint("bg isolate received: $data");
+    });
+  }
+
+  ForegroundService.sendToPort("message from bg isolate");
+
+}
+
+class AlarmInfo{
+  DateTime _alarmDate;
+  TimeOfDay _alarmTime;
+  bool _isAlarmOn;
+
+  AlarmInfo(DateTime date, TimeOfDay time, bool on){
+    this. _alarmDate = date;
+    this._alarmTime=time;
+    this. _isAlarmOn=on;
+  }
+  setAlarmDate(DateTime date){
+    _alarmDate = date;
+  }
+  getAlarmDate(){
+    return _alarmDate;
+  }
+
+  setAlarmTime(TimeOfDay time){
+    _alarmTime = time;
+  }
+  setAlarmOn(bool on){
+    _isAlarmOn = on;
+  }
+
+  isAlarmOn() {
+    return _isAlarmOn;
+  }
+
+}
+
+class AlarmTile extends StatefulWidget {
+  final List<AlarmInfo> _alarmInfo;
+  final int _index;
+
+  AlarmTile(this._alarmInfo, thils._index);
+
+  @override
+  State<StatefulWidget> createState() => AlarmTileState();
+}
+
+class AlarmTileState extends State<AlarmTile>{
+  bool _isChecked;
+
+  @override
+  Widge build(BuildContext context){
+    if(widget._alarmInfo.isEmpty){
+      return ListTile(
+        title:Text('등록된 알람이 없습니다.'),
+        subtitle: Text('알람을 등록해주세요'),
+      );
+    }
+
+    _isChecked = widget._alarmInfo[widget._index]._isAlarmOn();
+    final f = new DateFormat('yyyy-MM-dd H:mm');
+    int counterForAlarmOn = 0;
+    return ListTile(
+      leading: Icon(
+        Icons.alarm,
+        size:40,
+      ),
+      title:
+        Text('${f.format(widget._alarmInfo[widget._index].getAlarmDate())}'),
+      subtitle: Text('알람'),
+      trailing: Switch(
+        value: _isChecked,
+        onChanged: (value) async{
+          setState((){
+            _isChecked = value;
+          });
+          widget._alarmInfo[widget._index]
+          .setAlarmOn(!widget._alarmInfo[widget._index]._isAlarmOn());
+          var alarmID = await getAlarmID(widget._index);
+          var alarmID = widget._index;
+          if(widget._alarmInfo[widget._index]._isAlarmOn()){
+            AndroidAlarmManager.oneShotAt
+              (widget._alarmInfo[widget._index].getAlarmDate(),
+            alarmID,
+            startAlarm,
+            exact:true,
+            );
+          } else {
+            AndroidAlarmManager.cancel(alarmID);
+            stopAlarm();
+          }
+
+          for(int i =0 ; i< widget._alarmInfo.length; i++){
+            if(widget._alarmInfo[i].isAlarmOn()==true){
+              counterForAlarmOn++;
+            }
+          }
+
+          if(counterForAlarmOn == 0){
+            ForegroundService.stopForegroundService();
+          } else {
+            startService(widget._alarmInfo[widget._index].getAlarmDate());
+            counterForAlarmOn = 0;
+          }
+          saveAlarmInfo(
+            widget._index,
+            widget._alarmInfo[widget._index].getAlarmDate(),
+              (widget._alarmInfo[widget._index].getAlarmTime()).format(context),
+            widget._alarmInfo[widget._index].isAlarmOn());
+
+        },
+      ),
+      onLongPress: () async {
+        await showAlertDialog(context, widget._alarmInfo,widget._index);
+        Phoenix.rebirth(context);
+      },
+
+
+    );
+
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Alarm Off"),
+      ),
+      body: Container(
+        Ailgnment: Alignment.center,
+        margin: EdgeInsets.all(150.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.blue,
+        ),
+        child: IconButton(
+          icon : Icon(Icons.alarm_off),
+          iconSize: 50.0,
+          color: Colors.white,
+          tooltip: 'Alarm Off',
+          onPressed: () async{
+            List<String> dateList = List();
+            var count = await getAlarmCount();
+            for (int i = 0; i < count; i++) {
+              var myFuture = await getAlarmInfo(i);
+              if (myFuture[3]) {
+    String date =
+    DateFormat('yyyy-MM-dd H:mm:ss').format(myFuture[1]) +
+    ' ' +
+    myFuture[2].format(context);
+    dateList.add(date);
+           }
+
+          }
+    dateList.sort((a, b) => a.compareTo(b));
+
+    int counterForAlarmOn = 0;
+    for (int i = 0; i < count; i++) {
+    var myFuture = await getAlarmInfo(i);
+    if (myFuture[3]) {
+    counterForAlarmOn++;
+    }
+    }
+
+    if (dateList.isNotEmpty) {
+    for (int i = 0; i < count; i++) {
+    var myFuture = await getAlarmInfo(i);
+    String date =
+    DateFormat('yyyy-MM-dd H:mm:ss').format(myFuture[1]) +
+    ' ' +
+    myFuture[2].format(context);
+    if (dateList[0] == date) {
+    await removeAlarmInfo(i, context);
+    counterForAlarmOn--;
+    if (counterForAlarmOn == 0) {
+    ForegroundService.stopForegroundService();
+    } else {
+    counterForAlarmOn = 0;
+    }
+    stopAlarm();
+    SystemNavigator.pop();
+    break;
+
+    }
+    }
+    }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+Future<bool> saveAlarmID(int listID, int alarmID) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return await prefs.setInt('alarm_id_'+listID.toString(), alarmID);
+}
+
+Future<int> getAlarmID(int listID) async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getInt('alarm_id'+ listID.toString()) ?? 100;
+
+}
+
+void saveAlarmCount(int count) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setInt('count', count);
+}
+
+Future<int> getAlarmCount() async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getInt('count')?? 0;
+}
+
+Future<bool> saveAlarmInfo(
+  int listID, DateTime date, String time, bool on) async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String dateStr = DateFormat('yyyy-MM-dd H:mm:ss').format(date);
+
+  List<String> alarmInfoList = List();
+  alarmInfoList.add(listID.toString());
+  alarmInfoList.add(dateStr);
+  alarmInfoList.add(on.toString());
+
+  return await prefs.setStringList(listID.toString(), alarmInfoList);
+}
+
+Future<List<dynamic>> getAlarmInfo(int id) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  List<String> defaultData = List();
+  String defaultDate = DateFormat('yyyy-MM-dd H:mm:ss').format(DateTime.now());
+  String defaultTime = '00:00';
+
+  defaultData.add('100');
+  defaultData.add(defaultDate);
+  defaultData.add(defaultTime);
+  defaultData.add('default');
+
+  List<String> loadData = prefs.getStringList(id.toString()) ?? defaultData;
+  List<dynamic> parseData = [100, 200, DateTime.now(), TimeOfDay.now(), false];
+
+  parseData[0] = int.parse(loadData[0]); // listID
+  parseData[1] = DateFormat('yyyy-MM-dd H:mm:ss')
+      .parse(loadData[1] ?? defaultDate); // DateTime
+  parseData[2] = TimeOfDay(
+      hour: int.parse(loadData[2].split(":")[0] ?? defaultTime.split(":")[0]),
+      minute: int.parse(
+          loadData[2].split(":")[1] ?? defaultTime.split(":")[1])); // TimeOfDay
+
+  if ((loadData[3] ?? 'false') == 'true') {
+    parseData[3] = true;
+  } else {
+    parseData[3] = false;
+  }
+
+  return parseData;
 }
